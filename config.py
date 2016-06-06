b3e07bac94ee34036fa5196cc05abcfe54f1efe1
Anuj Bansal <anubansal@ebay.com>
2016-06-06 19:52:45 +0530

    config file added

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+
+# Set the Twitter credentials
+twitter = dict(
+    key = os.environ.get('TWITTER_KEY', ''),
+    secret = os.environ.get('TWITTER_SECRET', ''),
+    access_token = os.environ.get('TWITTER_TOKEN', ''),
+    access_token_secret = os.environ.get('TWITTER_TOKEN_SECRET', '')
+)
+
+# Giphy API key
+giphy = dict(
+    key = os.environ.get('GIPHY_KEY', '')
+)

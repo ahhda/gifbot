62cca5367d6bfe15b0cf5f8e4d1c77b67992e014
Anuj Bansal <anubansal@ebay.com>
2016-06-10 20:45:20 +0530

    Added message service

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import json
 import requests
 app = Flask(__name__)
 
@@ -6,15 +7,45 @@ app = Flask(__name__)
 def hello():
     return 'Hello World'
 
-@app.route('/webhook/')#, methods = ['POST'])
+def send_text_message(recipient_id, message):
+    payload = {
+            'recipient': {
+                'id': recipient_id
+            },
+            'message': {
+                'text': message
+            }
+        }
+    access_token = "EAAIPDYHtMsoBAOYf6CfnA6dKsTWQQzZBO1Pq0rLxgmDLBEh5RCV4Slvne2swN0YVhkZCe9PyhZC9Imu43hHQITN1p5x71incdH5cv5alWkjKoqFfJE1pPwthSjcZA0GC4MfRZCzrlHizlsReusPi29s7iI9xZARyFNC9L6IpaZAcQZDZD"
+    base_url = (
+            "https://graph.facebook.com"
+            "/v2.6/me/messages?access_token={1}"
+        ).format(access_token)
+    result = requests.post(base_url, json=payload).json()
+    return result
+
+@app.route('/webhook/', methods=['GET', 'POST'])
 def verify():
-    try:
-        ok = request.args.get('hub.verify_token')
-        if ok == 'my_voice_is_my_password_verify_me':
-            return request.args.get('hub.challenge')
-    except:
-        pass
-    return 'Yeah Fine, verified'
+    if request.method == 'POST':
+        try:
+            ok = request.args.get('hub.verify_token')
+            if ok == 'my_voice_is_my_password_verify_me':
+                return request.args.get('hub.challenge')
+        except:
+            pass
+        return 'Yeah Fine, verified'
+    else:
+        output = request.json
+        event = output['entry'][0]['messaging']
+        for x in event:
+            if (x.get('message') and x['message'].get('text')):
+                message = x['message']['text']
+                recipient_id = x['sender']['id']
+                send_text_message(recipient_id, message)
+            else:
+                print "Nothing"
+                pass
+        return "success"
 
 if __name__ == '__main__':
     print "RUNNING APP"

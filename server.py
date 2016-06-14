f895a5bea5c0ce4eb80d05fc73e2e3fb7d5e53fc
Anuj Bansal <anubansal@ebay.com>
2016-06-14 12:40:27 +0530

    Added Element code

--- server.py
+++ server.py
@@ -9,6 +9,19 @@ app = Flask(__name__)
 giphy = giphypop.Giphy(api_key=config.giphy['key'])
 MAX_IMAGE_SIZE = 3072 * 1024
 
+class Element(object):
+
+    __acceptable_keys = ['title', 'item_url', 'image_url', 'subtitle']
+    def __init__(self, **kwargs):
+        for key in self.__acceptable_keys:
+            setattr(self, key, kwargs.get(key))
+
+    def to_json(self):
+        data = {}
+        for key in self.__acceptable_keys:
+            data[key] = getattr(self, key)
+        return json.dumps(data)
+
 @app.route('/')
 def hello():
     return 'Hello World'
